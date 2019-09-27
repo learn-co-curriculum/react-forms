e71f78762b3d890565dad1af28b59669451b96e9
maxwellbenton <maxwellbenton@gmail.com>
2019-09-27 15:29:46 -0400

    adds solution code

---
+++ src/components/DisplayData.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class DisplayData extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.formData.firstName}</h1>
+        <h1>{this.props.formData.lastName}</h1>
+      </div>
+    )
+  }
+}
+
+export default DisplayData
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent/>
   </div>,
   document.getElementById('root')
 );
