5aa4dc0bd1409cb3fee7c745df0f606607361011
jillbowen <jillian.b.short@gmail.com>
2021-03-21 18:26:33 -0700

    commit tree ab5ecde39dfa3e045f2c126508a50fe541c9acf3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
   </div>,
   document.getElementById('root')
 );
